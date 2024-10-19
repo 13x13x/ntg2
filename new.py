62e7809183e8b16275afd0d3d42c5ea3d543ac45
13x13x <153285185+13x13x@users.noreply.github.com>
2024-10-19 13:45:53 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,8 @@
-from pyrogram import filters
+from pyrogram import Client, filters
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+import requests
+from bot import app, users_collection
 
 # Command to broadcast a message (admin only)
 async def broadcast(client, message, users_collection, OWNER_ID):
