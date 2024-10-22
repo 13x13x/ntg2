090f40ea994cd2549dfe7e755bb53cb9d34a8001
13x13x <153285185+13x13x@users.noreply.github.com>
2024-10-22 17:46:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,7 +135,7 @@ async def start(client, message):
         print(f"User {user_id} already exists in the database")  # Debugging line
 
     if user and user.get('banned', False):  # Check if the user is banned
-        await message.reply("**ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ 🚫 ғʀᴏᴍ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ**")
+        await message.reply("**Important Notice:** The bot is currently unable to execute commands as expected\n\nPlease check **/why** for full information**")
         return
 
     # Welcome text without formatting
