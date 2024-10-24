bf1a03b91ebb0da9ea4795a5d2d488d3a34f6f98
13x13x <153285185+13x13x@users.noreply.github.com>
2024-10-24 08:24:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -266,7 +266,7 @@ non_numeric_pattern = re.compile(r'[^\d.]')
 # Load the Roboto font once
 try:
     font_path = "fonts/Roboto-Bold.ttf"  # Ensure this path is correct
-    font_size = 10
+    font_size = 17
     font = ImageFont.truetype(font_path, font_size)
 except IOError:
     print("Error: Font file not found. Please check the font path.")
